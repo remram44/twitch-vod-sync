490db6b7fe3d251d0760402492195f4319a8913d
Remi Rampin <r@remirampin.com>
2020-10-05 22:54:58 -0400

    Fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import { App } from './App';
 
-test('renders learn react link', () => {
+test('renders redirect to Twitch auth', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Redirecting you to Twitch to authorize/i);
   expect(linkElement).toBeInTheDocument();
 });
