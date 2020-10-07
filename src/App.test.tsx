843f8333137ab98e16c88443a31cc452d320824a
Remi Rampin <r@remirampin.com>
2020-10-07 16:45:39 -0400

    Add tests for formatDelay()

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { App } from './App';
-import { computeDelay } from './utils';
+import { computeDelay, formatDelay } from './utils';
 
 test('renders redirect to Twitch auth', () => {
   const { getByText } = render(<App />);
@@ -10,9 +10,16 @@ test('renders redirect to Twitch auth', () => {
 });
 
 test('parsing delay', () => {
-  expect(computeDelay('1:23:45')).toBe(3600 + 23 * 60 + 45);
+  expect(computeDelay('1:03:45')).toBe(3600 + 3 * 60 + 45);
   expect(computeDelay('-1:00:45')).toBe(-(3600 + 45));
   expect(computeDelay('-12:45')).toBe(-(12 * 3600 + 45 * 60));
   expect(computeDelay('4:31')).toBe(4 * 3600 + 31 * 60);
   expect(computeDelay('-5m')).toBe(-5 * 60);
+});
+
+test('formatting delay', () => {
+  expect(formatDelay(3600 + 3 * 60 + 45)).toBe('01:03:45');
+  expect(formatDelay(-(3600 + 45))).toBe('-01:00:45');
+  expect(formatDelay(-(12 * 60 + 45))).toBe('-12:45');
+  expect(formatDelay(4 * 60 + 31)).toBe('04:31');
 });
